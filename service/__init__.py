1f20edf75d186462a5f3332e35d80a84d3e40892
Kunal Patel <kr391@stern.nyu.edu>
2020-04-12 14:19:54 -0400

    Updated the index to display proper message

--- service/__init__.py
+++ service/__init__.py
@@ -20,24 +20,24 @@ from service import service, models
 if __name__ != '__main__':
     GUNICORN_LOGGER = logging.getLogger('gunicorn.error')
     app.logger.handlers = GUNICORN_LOGGER.handlers
-    app.logger.setLevel(GUNICORN_LOGGER.level)
+    app.logger.setLevel(GUNICORN_LOGGER.level)# pylint: disable=maybe-no-member
     app.logger.propagate = False
     # Make all log formats consistent
-    formatter = logging.Formatter("[%(asctime)s] [%(levelname)s] [%(module)s] %(message)s",
+    formatter = logging.Formatter("[%(asctime)s] [%(levelname)s] [%(module)s] %(message)s", # pylint: disable=maybe-no-member
                                   "%Y-%m-%d %H:%M:%S %z") # pylint: disable=locally-disabled, invalid-name
     for handler in app.logger.handlers:
         handler.setFormatter(formatter)
-    app.logger.info('Logging handler established')
+    app.logger.info('Logging handler established') # pylint: disable=maybe-no-member
 
-app.logger.info(70 * "*")
-app.logger.info("  O R D E R   S E R V I C E   R U N N I N G  ".center(70, "*"))
-app.logger.info(70 * "*")
+app.logger.info(70 * "*") # pylint: disable=maybe-no-member
+app.logger.info("  O R D E R   S E R V I C E   R U N N I N G  ".center(70, "*")) # pylint: disable=maybe-no-member
+app.logger.info(70 * "*") # pylint: disable=maybe-no-member
 
 try:
     service.init_db()  # make our sqlalchemy tables
 except Exception as error:
-    app.logger.critical("%s: Cannot continue", error)
+    app.logger.critical("%s: Cannot continue", error) # pylint: disable=maybe-no-member
     # gunicorn requires exit code 4 to stop spawning workers when they die
     sys.exit(4)
 
-app.logger.info("Service inititalized!")
+app.logger.info("Service inititalized!")# pylint: disable=maybe-no-member
